efc1a62286421553eeb8f60886b3658c09f4e055
Josevdr95 <119988266+josevdr95new@users.noreply.github.com>
2025-08-12 18:33:47 -0400

    auw

--- sfl_bot/main.py
+++ sfl_bot/main.py
@@ -26,7 +26,8 @@ def setup_application() -> Application:
         CommandHandler("status", bot.handle_status),
         CommandHandler("calc", bot.handle_calc),
         CommandHandler("land", bot.handle_land),
-        MessageHandler(filters.COMMAND & ~filters.Regex(r"^/(start|help|status|calc|land)$"), bot.handle_item)
+        CommandHandler("auw", bot.handle_auw),
+        MessageHandler(filters.COMMAND & ~filters.Regex(r"^/(start|help|status|calc|land|auw)$"), bot.handle_item)
     ])
     
     application.add_error_handler(bot.error_handler)
